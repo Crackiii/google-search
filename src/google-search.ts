9acb46d3a850a787c85e88a0dc9d6d5750ccabe9
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-31 23:11:04 +0200

    add more proxies

--- src/google-search.ts
+++ src/google-search.ts
@@ -5,17 +5,45 @@ import { JSDOM } from "jsdom";
 import { Page } from "puppeteer";
 
 
-const proxies = [
+const proxies_list = [
   "165.231.37.254:7777",
   "165.231.37.98:7777  ",
   "185.104.218.48:7777 ",
   "185.104.219.37:7777 ",
-  "196.245.244.231:7777 "
+  "196.245.244.231:7777 ",
+  "185.121.138.57:7777",
+  "163.172.247.140:7777",
+  "104.227.255.22:7777",
+  "178.17.170.95:7777",
+  "165.231.35.99:7777",
+  "104.227.253.223:7777",
+  "104.227.255.56:7777",
+  "165.231.35.27:7777",
+  "196.196.160.65:7777",
+  "163.172.247.143:7777",
+  "165.231.35.66:7777",
+  "198.240.91.104:7777",
+  "185.121.138.32:7777",
+  "185.121.138.101:7777",
+  "198.240.90.113:7777",
+  "195.140.212.195:7777",
+  "195.140.212.134:7777",
+  "196.196.160.12:7777",
+  "165.231.35.96:7777",
+  "196.196.160.27:7777",
 ];
+
+const getProxy = () => {
+  const proxy = proxies_list.shift();
+  proxies_list.push(proxy);
+
+  return proxy;
+};
+
 
 export const getGoogleSearchResultsByQueries = async (queries: string[]) => {
   try {
-  const proxy = proxies[Math.floor(Math.random() * proxies.length)];
+  const proxy = getProxy();
 
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_PAGE,
