d60674dc0af952d921f6fb511aa6e8af3b0f37f8
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-04-25 11:46:32 +0200

    meaning ful message

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,10 @@ if (process.env.NODE_ENV === "development") {
 
 const server = app.listen(app.get("port"), () => {
     console.log(
-        "The wow is running at %d in %s mode",
+        "[Trendscads Scrapper Backend]: Running at %d in %s mode. (%s)",
         app.get("port"),
-        app.get("env")
+        app.get("env"),
+        new Date().toLocaleString()
     );
 });
 
